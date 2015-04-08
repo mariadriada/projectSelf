f343590d105506b7a43829061fa184a224e02df7
Maria <mariagiraldo4@gmail.com>
2015-04-07 19:28:35 -0500

    Adding initial files

---
+++ app/scripts/src/app.js
@@ -0,0 +1,276 @@
+//Angular application
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider){
+
+    $routeProvider
+    	
+    	.when('/home',{
+            templateUrl: "views/home.html",
+            controller: "t_indexCtrl"
+        })
+
+      
+        //Default
+        .otherwise({
+            redirectTo: '/home'
+        })
+});
+
+//Index controller
+app.controller('t_indexCtrl', function($scope){
+	console.log('t_indexCtrl'); 
+});
+
+app.controller('t_bannerCtrl', function($scope){
+    console.log('t_bannerCtrl'); 
+});
+
+
+
+//Directive for Slider component
+app.directive('sliderChangeBgc', function() {
+  return {
+    restrict: "E",
+    transclude:true,
+    scope: {
+        data1: "="
+    },
+    templateUrl: 'views/ui-components/sliderChangeBgc.html',
+    replace: true
+  };
+});
+
+//Directive for access component
+app.directive('access', function() {
+  return {
+    restrict: "E",
+    transclude:true,
+    scope: {
+        data: "="
+    },
+    templateUrl: 'views/ui-components/access.html',
+    replace:true
+  };
+});
+
+//Directive for maps
+app.directive('maps', function() {
+  return {
+    restrict: "E",
+    transclude:true,
+    scope: {
+        data: "="
+    },
+    templateUrl: 'views/ui-components/maps.html',
+    replace:true
+  };
+});
+
+
+
+// Directive for information backgroung-image box 
+app.directive('boxInformationBgi', function() {
+  return {
+    restrict: "E",
+    transclude:true,
+    scope: {
+        title1: "@",
+        title2: "@",
+        title3: "@",
+        icontext: "@",
+        img:"@",
+        classopacity:  "@",
+        class1: "@",
+        class2: "@",
+        class3: "@",
+        class4: "@",
+        type: "@"
+    },
+    templateUrl: 'views/ui-components/boxInformationBgi.html',
+    replace:true,
+    link: function(){
+        console.log("boxInformationBgi");
+    }
+  };
+});
+
+
+
+//Directive for plane slider component
+app.directive('sliderPlane', function() {
+  return {
+    restrict: "E",
+    transclude:true,
+    scope: {
+        title: '@',
+        description: '@',
+        options: "="
+    },
+    link: function(scope, element, attrs){
+        console.log("link slide plane ");
+
+        scope.data = {
+        articles: [
+            { 
+                "title1": "CELEBRATION" , 
+                "title2": "Sensual",
+                "img": "resources/img/article1.jpg",
+                "icontext": "&",
+                "class1": "title title1 font-monteserrat",
+                "class2": "title  fonts-roboto icon-text",
+                "class3": "title title2 fonts-roboto",
+                "classopacity": "red",
+
+            }, 
+            { 
+                "title1":   "Beauty" , 
+                "title2":   "for two",
+                "title3":   "THIS MONTH, WE GIVE YOU A BEAUTY SESSION FOR THE PURCHASE ANOTHER THER. 70C." , 
+                "img":      "resources/img/article2.jpg",
+                "class1":   "font-droid-serif f60 fw500 t1",
+                "class3":   "title f22 fonts-roboto t2 ls2 ",
+                "class4":   "title t3 f18 ls4",
+                "classopacity": "green-1",
+            },
+             { 
+                "title1": "ESCAPADA" , 
+                "title2": "ACTIVA'T 20C.",
+                "img": "resources/img/article3.jpg",
+                "class1":   "title title1 font-monteserrat",
+                "class3":   "title  fonts-roboto t4 ",
+                "classopacity": "violet",
+            }, 
+            { 
+                "img": "resources/img/article4.jpg",
+                "classopacity": "blue",
+            }, 
+            { 
+                "maps": "<maps></maps>",
+            }, 
+            { 
+                "sci": "<slider-change-information></slider-change-information>",
+            }, 
+            { 
+                "follow": "<follow-us></follow-us>",
+            }
+        ]
+    }
+        console.log(scope);
+    },
+    templateUrl: 'views/ui-components/sliderPlane.html',
+    replace:true
+  };
+});
+
+//Directive for information box
+app.directive('boxInformation', function() {
+  return {
+    restrict: "E",
+   transclude:true,
+   scope: {
+        data1: "="
+    },
+    templateUrl: 'views/ui-components/boxInformation.html'
+  };
+});
+
+
+// Directive for information box-maps
+app.directive('boxMaps', function() {
+  return {
+    restrict: "E",
+    transclude:true,
+    scope: {
+        type: "@"
+    },
+    templateUrl: 'views/ui-components/boxMaps.html',
+    replace:true,
+    link: function(){
+        console.log("boxMaps");
+    }
+  };
+});
+
+
+// Directive for information slider-change-information
+app.directive('sliderChangeInformation', function() {
+  return {
+    restrict: "E",
+    transclude:true,
+    scope: {
+        type: "@"
+    },
+    templateUrl: 'views/ui-components/sliderChangeInformation.html',
+    replace:true,
+    link: function(){
+        console.log("sliderChangeInformation");
+    }
+  };
+});
+
+
+// Directive for information follow-us
+app.directive('followUs', function() {
+  return {
+    restrict: "E",
+    transclude:true,
+    scope: {
+        type: "@"
+    },
+    templateUrl: 'views/ui-components/followUs.html',
+    replace:true,
+    link: function(){
+        console.log("followUs");
+    }
+  };
+});
+
+
+
+
+
+/*fn jquery*/
+
+
+
+$(document).ready(function() {
+   
+    //Click on buttons main slider
+    $(".icon-slider").click(function(){
+        var type = $(this).attr('type');
+        if (type === 'next') { i++; }
+        else { i--; }
+
+         //number static of images
+        if (i === 3) { i= 0; } else if (i === -3) { i= 0;}
+        
+     
+        $("#slider img").hide();
+        $("#slider img").eq(i).fadeIn("medium");
+        
+    });
+});
+
+
+
+function changeColor(element){
+    var color = $(element).attr("color");
+    var obj = $(element).attr("element");
+    $(obj).css("background-color",color);
+    console.log("change "+color +" element "+obj);
+};
+
+
+var i = 0;
+function imageChange(type) {
+    i++;   
+    $("#slider img").hide();
+    $("#slider img").eq(i).fadeIn();   
+    
+    if ( i == $("slider img").size()){
+        i = 0;
+    }
+}   
+
+//setInterval("imageChange('next')", 3000);
---
+++ gulpfile.js
@@ -0,0 +1,128 @@
+var	gulp 		   = 	require('gulp'),
+	browserSync  = 	require('browser-sync'),
+	sass		     =	require('gulp-sass'),
+	prefix       =	require('gulp-autoprefixer'),
+	concat       = 	require('gulp-concat'),
+	cssmin       = 	require('gulp-minify-css'),
+	uglify       = 	require('gulp-uglify'),
+  bower        =  require('gulp-bower'),
+	reload       =	 browserSync.reload
+	;
+
+//Browser sync
+gulp.task('browser-sync', ['sass'], function () {
+  browserSync({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+
+//Takes main.scss, add the prefixes and set the compiled file in the css folder.
+gulp.task('sass', function(){
+	gulp.src('./app/scss/main.scss')
+    .pipe(sass({      
+      onError: browserSync.notify
+    }))
+    .pipe(prefix(['last 3 versions', '> 1%'], { cascade: true }))
+    .pipe(gulp.dest('./app/css/'))
+     .pipe(browserSync.reload({stream: true}));
+
+     
+});
+
+
+// Compile files from scss into minified css
+gulp.task('sassmin', function () {
+  gulp.src('./app/css/main.css')    
+    .pipe(prefix(['last 3 versions', '> 1%'], { cascade: true }))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./app/css/min/'));
+});
+
+
+// Minifies main.js
+gulp.task('uglify', function () {
+  gulp
+    .src('./app/scripts/src/*.js')   
+    .pipe(uglify())
+    .pipe(gulp.dest('./app/scripts/min/'));
+});
+
+// Concat the scripts in the src folder.
+gulp.task('scripts', function () {
+  gulp
+    .src(['./app/scripts/src/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./app/scripts/'));
+});
+
+//Html templates on public directory for production
+gulp.task('public-html', function(){
+	 gulp
+    .src(['./app/**/*.html'])
+    .pipe(gulp.dest('./public/'));
+});
+
+//Scripts on public directory for production
+gulp.task('public-scripts', function(){
+	 gulp
+    .src(['./app/scrips/src/main.js'])
+    .pipe(gulp.dest('./public/scripts/src/'));
+});
+
+//Style on public directory for production
+gulp.task('public-style', function(){
+	 gulp
+    .src(['./app/css/**/*.css'])
+    .pipe(gulp.dest('./public/css/'));
+});
+
+//Resources on public directory for production
+gulp.task('public-resources', function(){
+   gulp
+    .src(['./app/resources/**/*'])  //Copy directories and files
+    .pipe(gulp.dest('./public/resources/'));  //Paste on public directory
+
+});
+
+//Scripts on public directory for production
+gulp.task('public-scripts', function(){
+   gulp
+    .src(['./app/scripts/**/*'])  //Copy directories and files
+    .pipe(gulp.dest('./public/scripts/'));  //Paste on public directory
+});
+
+
+//Watch
+gulp.task('watch', function(){
+	//Reload Templates and Scripts
+	gulp.watch(['./app/*.html', 'app/scripts/src/*.js', './app/views/**/*.html'], reload);
+	gulp.watch('./app/scss/*.scss', ['sass', 'sassmin']);  
+	gulp.watch('./app/scripts/src/*.js', ['scripts', 'uglify']);
+
+});
+
+//Bower: for downloading packages for development front-end
+gulp.task('bower', function() {
+  return bower('./my_bower_components')
+    .pipe(gulp.dest('lib/'))
+});
+
+
+//DEFAULT TASK
+gulp.task('default', ['serve']);
+
+
+//Serve task
+gulp.task('serve', ['browser-sync', 'watch']);
+
+//Production Task 
+gulp.task('production', ['sassmin', 'uglify', 'public-html', 'public-scripts', 'public-style', 'public-resources', 'public-scripts']);
+
+//Build task
+gulp.task('build', ['sass', 'scripts', 'uglify']);
+
+
+
